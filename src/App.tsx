e82456859a560e4477545bb19d9b2689bbd0a882
Thiago Munich <thiagomunich@gmail.com>
2022-10-19 10:28:05 -0300

    fix: removing peer-dependencies from deploy-docs.yml

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import { Heading } from "./components/Heading/Heading"
 import { Text } from "./components/Text/Text"
+import { Logo } from "./Icons/Logo"
 import "./styles/global.css"
 
 export function App() {
   return (
-    <div className="w-screen h-screen flex items-center justify-center flex-col bg-gray-800">
-      <Heading>Teste</Heading>
+    <div className="w-screen h-screen flex items-center justify-center flex-col bg-gray-800 text-gray-100">
+      <Logo />
 
-      <Text size="lg">teste</Text>
+      <Heading size="lg">Ignite Lab</Heading>
+
+      <Text size="lg">Login and start using</Text>
     </div>
   )
 }
