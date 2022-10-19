5b893bc5b3341acf90a25a763aea9c480514806f
Thiago Munich <thiagomunich@gmail.com>
2022-10-19 10:13:23 -0300

    feat: adding a11y addon to storybook

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,10 @@ import "./styles/global.css"
 
 export function App() {
   return (
-    <>
+    <div className="w-screen h-screen flex items-center justify-center flex-col bg-gray-800">
       <Heading>Teste</Heading>
 
       <Text size="lg">teste</Text>
-    </>
+    </div>
   )
 }
